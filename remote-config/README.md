9acb53b1f465e495bad2d8514d8153b0b4e9baa6
Tuấn Đỗ <tuan.do1@onemount.com>
2022-11-04 17:02:08 +0700

    Update remote config demo

--- remote-config/README.md
+++ remote-config/README.md
@@ -2,10 +2,16 @@
 
 [Add Firebase to your JavaScript project](https://firebase.google.com/docs/web/setup?authuser=0#add-sdk-and-initialize)
 
+[Get started with Firebase Remote Config](https://firebase.google.com/docs/remote-config/get-started?platform=web&authuser=0#rest_1)
+
 ## Cấu hình
 **Bước 1:** Truy cập vào [Google console](https://console.firebase.google.com/), tạo một project mới (nếu có rồi thì khỏi tạo cũng được)
 
-**Bước 2:** Vào phần Remote Config cấu hình Parmaeters và Conditions
+**Bước 2:** Lấy thông tin cấu hình của project trong [project settings](https://console.firebase.google.com/project/_/settings/general/)
+
+Tham khảo: https://support.google.com/firebase/answer/7015592
+
+**Bước 3:** Vào phần Remote Config cấu hình Parameters và Conditions
 
 
 
--- remote-config/src/App.tsx
+++ remote-config/src/App.tsx
@@ -1,36 +1,33 @@
 // Libraries
 import React, {useEffect} from 'react';
 import {initializeApp} from 'firebase/app';
-import {collection, getDocs, getFirestore} from 'firebase/firestore/lite';
+import { getRemoteConfig, getValue, getAll, getString, getBoolean, fetchAndActivate } from "firebase/remote-config";
+
 
 const MyApp = () => {
     const firebaseConfig = {
         apiKey: "AIzaSyB_7npAIvYGXq_970-0pzHa0sF56mE9PcA",
-        // authDomain: "PROJECT_ID.firebaseapp.com",
-        // The value of `databaseURL` depends on the location of the database
-        // databaseURL: "https://DATABASE_NAME.firebaseio.com",
+        authDomain: "agile-antler-210706.firebaseapp.com",
+        databaseURL: "https://agile-antler-210706.firebaseio.com",
         projectId: "agile-antler-210706",
-        // storageBucket: "PROJECT_ID.appspot.com",
-        // messagingSenderId: "SENDER_ID",
-        appId: "168906573103",
-        // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-        // measurementId: "G-MEASUREMENT_ID",
+        storageBucket: "agile-antler-210706.appspot.com",
+        messagingSenderId: "168906573103",
+        appId: "1:168906573103:web:98f81b120bb9458e4ccdda",
+        measurementId: "G-291N1VBG7V"
     };
 
+    // Initialize Firebase
     const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
-
-    async function getCities(db) {
-        const citiesCol = collection(db, 'cities');
-        const citySnapshot = await getDocs(citiesCol);
-
-        return citySnapshot.docs.map(doc => doc.data());
-    }
+    const remoteConfig = getRemoteConfig(app);
+    remoteConfig.settings.minimumFetchIntervalMillis = 2;
 
     useEffect(() => {
-        getCities(db).then((response) => {
-            console.log('response', response)
-        })
+        fetchAndActivate(remoteConfig)
+            .then(() => {
+                const val = getValue(remoteConfig, 'corner');
+
+                console.log('val', val)
+            })
     }, [])
 
     return <>MyApp</>;
